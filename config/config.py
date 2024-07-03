26b3872a209db3cf59fbc9d4148372817a37ea4b
PinguimIdoneo <penguinnation94@gmail.com>
2024-07-03 09:59:47 +0000

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,8 @@
 import os
 
 class Config:
-    # Default settings
     SECRET_KEY = os.getenv('SECRET_KEY', 'w5Ib0ARwVLbS0rSwJpzZRA')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Use SQLite as fallback if DATABASE_URL is not set
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://pnationtools_user:cBbTguDKychOxL38ThBPillXNSPUXWHz@dpg-cq27gd56l47c73b124l0-a/pnationtools')
 
 class DevelopmentConfig(Config):
@@ -17,13 +14,4 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    # Ensure the environment variable is set for production database
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
-
